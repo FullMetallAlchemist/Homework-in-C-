8e47cbc1618c6d5fc3450233f9dfd73520271b38
Alchemist <viktor-avas@yandex.ru>
2022-10-13 21:44:38 +0300

    added two examples

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1 Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+
+/*
+Console.Write("Input a positive integer number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int current = - number;
+while(current <= number)
+{
+    Console.Write(current + " ");
+    current++; 
+}
+*/
+
+// Задача 2 Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+
+Console.Write("Input three-digit number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = number % 10;
+Console.WriteLine($"Last digit of {number} is {result}");
+
+
+
