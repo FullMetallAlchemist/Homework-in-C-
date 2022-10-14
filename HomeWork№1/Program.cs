cece35a996498325ec92bbf336b7db732f9745b8
Alchemist <viktor-avas@yandex.ru>
2022-10-14 15:02:38 +0300

    added 4 tasks in hm1

--- HomeWork№1/Program.cs
+++ HomeWork№1/Program.cs
@@ -21,33 +21,43 @@ Console.Write("Input second number: ");
 int number_second = Convert.ToInt32(Console.ReadLine()); //Вводим и присваиваем второе число
 Console.Write("Input third number: ");
 int number_third = Convert.ToInt32(Console.ReadLine()); //Вводим и присваиваем третье число
-int max = 0;
-if (number_first > number_second)
+int max = number_first;
+
+if (number_second > max)                                //Сравниваем числа
+ max = number_second;
+
+if (number_third > max)
+ max = number_third; 
+
+Console.WriteLine($"The maximum of the entered numbers is {max}"); // Выводим результат
+    
+*/
+
+//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+/*
+Console.Write("Input one number: "); 
+int number = Convert.ToInt32(Console.ReadLine()); //Получаем от пользователя число
+if(number%2 == 0) // Проверяем число на чётность.
+Console.WriteLine($"The entered number {number} is even");
+else
+Console.WriteLine($"The entered number {number} is odd");
+*/
+
+//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+/*
+Console.Write("Input one integer number: ");    //Запрашиваем число у пользователя
+int number = Convert.ToInt32(Console.ReadLine());
+int count = 1;                                  //Делаем счётчик
+if (number < 2)
+Console.WriteLine("There are no even numbers in the list from 1 to N.");
+while(number > 1)
 {
-    if(number_first > number_third)
+    if(number%2==0)
     {
-        max = number_first;
-        Console.WriteLine($"The maximum of the entered numbers is {max}");
+        Console.Write(number + " " );
+        
     }
-}
-if (number_second > number_first)
-{
-    if(number_second > number_third)
-    {
-        max = number_second;
-        Console.WriteLine($"The maximum of the entered numbers is {max}");
-    }
-}
-if (number_third > number_first)
-{
-    if(number_third> number_second)
-    {
-        max = number_third;
-        Console.WriteLine($"The maximum of the entered numbers is {max}");
-    }
+    number = number - count;
 }
 */
-    
-
-
 
