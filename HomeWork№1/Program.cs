8d7a6207824d856eacedd5f03629d7467b5ab7b1
Alchemist <viktor-avas@yandex.ru>
2022-10-14 13:31:33 +0300

    added hm1

---
+++ HomeWork№1/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+/*
+Console.WriteLine("Input two integer numbers"); 
+Console.Write("Input first number: ");
+int number_first = Convert.ToInt32(Console.ReadLine()); //Вводим и присваиваем первое число
+Console.Write("Input second number: ");
+int number_second = Convert.ToInt32(Console.ReadLine()); //Вводим и присваиваем первое число
+
+if (number_first > number_second) //Сравнение двух чисел
+Console.WriteLine($"{number_first} more then {number_second}");
+else
+Console.WriteLine($"{number_second} more then {number_first}!");
+*/
+
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+/*
+Console.WriteLine("Input three integer numbers");
+Console.Write("Input first number: ");
+int number_first = Convert.ToInt32(Console.ReadLine()); //Вводим и присваиваем первое число
+Console.Write("Input second number: ");
+int number_second = Convert.ToInt32(Console.ReadLine()); //Вводим и присваиваем второе число
+Console.Write("Input third number: ");
+int number_third = Convert.ToInt32(Console.ReadLine()); //Вводим и присваиваем третье число
+int max = 0;
+if (number_first > number_second)
+{
+    if(number_first > number_third)
+    {
+        max = number_first;
+        Console.WriteLine($"The maximum of the entered numbers is {max}");
+    }
+}
+if (number_second > number_first)
+{
+    if(number_second > number_third)
+    {
+        max = number_second;
+        Console.WriteLine($"The maximum of the entered numbers is {max}");
+    }
+}
+if (number_third > number_first)
+{
+    if(number_third> number_second)
+    {
+        max = number_third;
+        Console.WriteLine($"The maximum of the entered numbers is {max}");
+    }
+}
+*/
+    
+
+
+
